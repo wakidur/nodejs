74c415e8b7e36868ec47b2c5a81d431c480e206e
Wakidur probAZe <wakidur@gmail.com>
2016-04-09 18:00:50 +0600

    add some fiele

--- nodeproject/p3/app.js
+++ nodeproject/p3/app.js
@@ -1,7 +1,12 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello Wakidur it is you first node project');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
 
 
